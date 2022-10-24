f0db8267c9332d58830c5db65a48d2aaf20685f7
Jeremy Muriel <jeremy@jeremm.fr>
2022-10-24 09:26:57 +0200

    bump golangci-lint to v1.45

--- jdecode_test.go
+++ jdecode_test.go
@@ -23,7 +23,6 @@ func TestDecodePassword(t *testing.T) {
 	}
 }
 
-// nolint: gosec
 // TestDecodeBadEncoded try decode encrypted secret but with missing or extra character.
 func TestDecodeBadEncoded(t *testing.T) {
 	// remove last character
@@ -31,12 +30,12 @@ func TestDecodeBadEncoded(t *testing.T) {
 		t.Errorf("missing character in junWordCoded not detected, passwordDecoded: %s", passwordDecoded)
 	}
 	// add last character
-	if passwordDecoded, err := junosdecode.Decode(junWordCoded + string(rune(rand.Intn(26)))); err == nil {
+	if passwordDecoded, err := junosdecode.Decode(junWordCoded + string(rune(rand.Intn(26)))); err == nil { //nolint: gosec
 		t.Errorf("extra character in junWordCoded not detected, passwordDecoded: %s", passwordDecoded)
 	}
 	// add two last characters
 	if passwordDecoded, err := junosdecode.Decode(
-		junWordCoded + string(rune(rand.Intn(26))) + string(rune(rand.Intn(26)))); err == nil {
+		junWordCoded + string(rune(rand.Intn(26))) + string(rune(rand.Intn(26)))); err == nil { //nolint: gosec
 		t.Errorf("extra characters in junWordCoded not detected, passwordDecoded: %s", passwordDecoded)
 	}
 }
